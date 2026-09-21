6c2228b924532a37576dd001fa1e5ad12a1ad4e2
Priya Sheoran <priya.sheoran1414@gmail.com>
2026-09-22 01:32:36 +0530

    Update SECRET_KEY to use environment variable
    Load SECRET_KEY from environment variable instead of hardcoding.

--- tonefit/settings.py
+++ tonefit/settings.py
@@ -20,8 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=&fi+nm(rt-50vmv5yt^b162e+qg87nchdzxk#r+r^$w3_frhz'
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
